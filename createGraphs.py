ea7930a01922cbd581071b8863699a5b1566dbbe
unknown <stevenkerr2@gmail.com>
2021-05-25 09:35:59 +0100

    _

--- createGraphs.py
+++ createGraphs.py
@@ -42,7 +42,7 @@ OWID = iD.Open('OWID')
 
 # The DailyHosp is updated daily.
 # The file is downloaded automatically.
-#iD.importDailyHosp()
+iD.importDailyHosp()
 
 newHospAdDiag = iD.Open('newHospAdDiag')
 
